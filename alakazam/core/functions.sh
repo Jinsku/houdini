c0025b97b9a597e62e979f0b7e8aa04223d0df32
brett <admin@krux.us>
2016-09-24 19:41:42 -0400

    Updated paths to use PWD global env since this is POSIX and it should always work.

--- alakazam/core/functions.sh
+++ alakazam/core/functions.sh
@@ -1,12 +1,3 @@
-#used to actually display .motd and whatnot
-display() {
-  if [ -e ~/houdini/${motdLeft} ]; then
-    export GREP_COLOR=35
-    paste ~/houdini/${motdLeft} | egrep --color -A5 "[^\"PKXYdb8\.\+\-]"
-    export GREP_COLOR='1;31' # set back to default
-  fi
-}
-
 #used to set tab titles in iTerm2 and similar clients
 setTerminalTitle() {
     echo -ne "\033]0;"$*"\007"
